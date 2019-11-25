ffd587f09a160baca6dbdff915f580617ccd8900
Nelson <nbritob@hotmail.com>
2019-11-25 16:23:43 +0000

    MVP Done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue ({
+    el: "#app",
+    data: {
+      items: [
+        {task:"Practice Sumo deadlifts", isDone: false},
+        {task:"Prep Gainzgiving for Sunday", isDone: false},
+        {task:"Do laundry", isDone: true},
+        {task:"Finish MVP", isDone: false},
+        {task:"Wrestle Alligator", isDone: true} ],
+        newItem: ""
+    },
+    methods: {
+      saveNewItem: function() {
+        this.items.push({
+          task: this.newItem,
+          isDone: false});
+          this.newItem = "";
+      },
+      doItem: function (index) {
+        this.items[index].isDone = true;
+      }
+    }
+  });
+});
